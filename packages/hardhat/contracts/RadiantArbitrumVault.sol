c14635d69ecbdb167cf48ad0b9e3b25613aea2bd
davidtnfsh <davidtnfsh@gmail.com>
2023-05-29 19:30:55 +0200

    [wip](radiant): does not work

--- packages/hardhat/contracts/RadiantArbitrumVault.sol
+++ packages/hardhat/contracts/RadiantArbitrumVault.sol
@@ -7,22 +7,25 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "hardhat/console.sol";
-// import "./radiant/ILendingPool.sol";
+import "./radiant/ILendingPool.sol";
 
 
 contract RadiantArbitrumVault is ERC4626 {
     IERC20 private immutable _underlying;
-    // ILendingPool public radiantLending;
-    // LendingPool public radiantLending;
+    ILendingPool public radiantLending;
     constructor(string memory name_, string memory symbol_, IERC20Metadata underlying_, address radiantLending_) 
         ERC4626(underlying_)
         ERC20(name_, symbol_) 
     {
         _underlying = underlying_;
-        // radiantLending = ILendingPool(radiantLending_);
+        radiantLending = ILendingPool(radiantLending_);
     }
-
-    // function deposit(uint256 amount, address onBehalfOf, uint16 referralCode) {
-    //     radiantLending.deposit(_underlying, amount, onBehalfOf, referralCode);
-    // }
+    function deposit(uint256 amount, address receiver) public override returns (uint256) {
+        uint256 shares = super.deposit(amount, receiver);
+        require(shares > 0, "erc4626 deposit failed");
+        _underlying.approve(address(radiantLending), amount);
+        // TODO(david): radiant doesn't work for now
+        // radiantLending.deposit(address(_underlying), amount, receiver, 0);
+        return shares;
+    }
 }
