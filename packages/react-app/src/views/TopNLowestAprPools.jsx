c6159ed4e24405f2ea02e3c16b298e2bc50abddf
davidtnfsh <davidtnfsh@gmail.com>
2023-06-05 18:45:11 +0200

    [fix](react): upload missing jsx

---
+++ packages/react-app/src/views/TopNLowestAprPools.jsx
@@ -0,0 +1,47 @@
+import { Table, Tag } from "antd";
+import { useState, useEffect } from "react";
+
+export default function TopNLowestAprPools(props) {
+  const { wording, topNData, portfolioAPR } = props;
+  const [data, setData] = useState([]);
+  const columns = [
+    {
+      title: "Pool",
+      dataIndex: "pool",
+      key: "pool",
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: "APR",
+      key: "apr",
+      dataIndex: "apr",
+      render: apr => {
+        let color = apr < portfolioAPR / 100 ? "volcano" : "green";
+        return (
+          <>
+            <Tag color={color} key={apr}>
+              {(apr * 100).toFixed(2)}%
+            </Tag>
+          </>
+        );
+      },
+    },
+  ];
+  useEffect(() => {
+    if (topNData) {
+      const extractedData = topNData.map((metadata, idx) => ({
+        key: idx,
+        pool: metadata.pool,
+        apr: metadata.apr,
+      }));
+      setData(extractedData);
+    }
+  }, [topNData]);
+
+  return (
+    <div>
+      <h3>{wording}:</h3>
+      <Table columns={columns} dataSource={data} />
+    </div>
+  );
+}
