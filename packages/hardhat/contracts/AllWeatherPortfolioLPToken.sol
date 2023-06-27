f7e972d8d03c278d5c73cd6fdb6b310b65cea063
davidtnfsh <davidtnfsh@gmail.com>
2023-06-27 11:13:07 +0200

    [doc]: add comments

--- packages/hardhat/contracts/AllWeatherPortfolioLPToken.sol
+++ packages/hardhat/contracts/AllWeatherPortfolioLPToken.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
-// 1. LP token has shares of radiant vault
-// 2. shoudl have lp token
-// 3. all weather vault is erc4626
+// The code defines a Solidity contract called AllWeatherPortfolioLPToken that inherits from ERC20. It takes in several parameters on construction, including asset, radiantVaultAddr, and dpxVaultAddr. The contract has several functions that do the following:
+
+// deposit: Takes in an amount and transfers tokens of asset from the user to the contract, then distributes the asset into two protocols (DPX and Radiant) based on a portfolioAllocation. The user receives an ERC20 token (AWVLP) in proportion to their deposit.
+// redeemAll: Takes in a number of shares and an account, then redeems all DPX LP Tokens and sends them to the account. Only DPX LP tokens are redeemed. The proportion of redeemed tokens is distributed to the sender's ERC20 tokens (AWVLP).
+// claimableRewards: Takes in an account, calculates the user's claimable rewards across both protocols and returns them.
+// claim: Takes in an account and reward tokens, and claims all the available rewards across both protocols, sending them to the account.
+// The code imports several open source libraries and uses various data structures like struct, bytes, and mapping. The SPDX-License-Identifier specifies the license for the code (MIT in this case).
 
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
