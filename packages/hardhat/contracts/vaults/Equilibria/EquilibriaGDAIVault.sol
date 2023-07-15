de7a22c83d7b9ff1ba331be1303a9b487520a8e5
davidtnfsh <davidtnfsh@gmail.com>
2023-07-15 12:04:05 +0200

    test slither

--- packages/hardhat/contracts/vaults/Equilibria/EquilibriaGDAIVault.sol
+++ packages/hardhat/contracts/vaults/Equilibria/EquilibriaGDAIVault.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.4;
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./BaseEquilibriaVault.sol";
 
 contract EquilibriaGDAIVault is BaseEquilibriaVault {
