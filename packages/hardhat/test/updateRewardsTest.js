fc0c2705d74aa182987d8b125162503e646d0953
davidtnfsh <davidtnfsh@gmail.com>
2023-08-01 10:15:22 +0200

    [test](reward): added test case for updateRewards(), part2

--- packages/hardhat/test/updateRewardsTest.js
+++ packages/hardhat/test/updateRewardsTest.js
@@ -166,10 +166,34 @@ describe("All Weather Protocol", function () {
                 }
             }
         });
-        // it("userRewardsOfInvestedProtocols should be reset to 0 after claim()", async function () {
-        //     await (await portfolioContract.connect(wallet).claim(randomWallet.address, { gasLimit: 30000000 })).wait();
-        //     expect(userRewardsOfInvestedProtocols).to.equal(0);
-        // })
+        it("userRewardsOfInvestedProtocols should be reset to 0 after claim()", async function () {
+            expect(await portfolioContract.rewardPerShareZappedIn(radiantVault.name(), radiantRTokens[0])).to.equal(0);
+            await deposit(wallet);
+            const rewardPerShareZappedIn1 = await portfolioContract.rewardPerShareZappedIn(radiantVault.name(), radiantRTokens[0]);
+            expect(rewardPerShareZappedIn1).to.be.gt(0);
+            await mineBlocks(1700); // wait for 7 hours, otherwise the reward/shares would be too small and be rounded to 0
+            await deposit(wallet2);
+            const rewardPerShareZappedIn2 = await portfolioContract.rewardPerShareZappedIn(radiantVault.name(), radiantRTokens[0]);
+            expect(rewardPerShareZappedIn2).to.be.gt(rewardPerShareZappedIn1);
+
+            // claim
+            expect(await portfolioContract.userRewardPerTokenPaid(wallet.address, radiantVault.name(), radiantRTokens[0])).to.equal(0);
+            await (await portfolioContract.connect(wallet).claim(wallet.address, { gasLimit: 30000000 })).wait();
+            expect(await portfolioContract.userRewardPerTokenPaid(wallet.address, radiantVault.name(), radiantRTokens[0])).to.equal(await portfolioContract.rewardPerShareZappedIn(radiantVault.name(), radiantRTokens[0]));
+            expect(await portfolioContract.userRewardsOfInvestedProtocols(wallet.address, radiantVault.name(), radiantRTokens[0])).to.equal(0);
+            expect(await portfolioContract.userRewardsOfInvestedProtocols(wallet2.address, radiantVault.name(), radiantRTokens[0])).to.equal(0);
+
+            // 2nd deposit for wallet2
+            await weth.connect(wallet2).deposit({ value: ethers.utils.parseEther("0.1"), gasLimit: 2057560 });
+            await deposit(wallet2);
+            expect(await portfolioContract.userRewardsOfInvestedProtocols(wallet2.address, radiantVault.name(), radiantRTokens[0])).to.be.gt(0);
+            await (await portfolioContract.connect(wallet2).claim(wallet2.address, { gasLimit: 30000000 })).wait();
+            expect(await portfolioContract.userRewardsOfInvestedProtocols(wallet2.address, radiantVault.name(), radiantRTokens[0])).to.equal(0);
+            expect(await portfolioContract.userRewardPerTokenPaid(wallet2.address, radiantVault.name(), radiantRTokens[0])).to.equal(await portfolioContract.rewardPerShareZappedIn(radiantVault.name(), radiantRTokens[0]));
+            const rewardPerShareZappedIn3 = await portfolioContract.rewardPerShareZappedIn(radiantVault.name(), radiantRTokens[0]);
+            expect(rewardPerShareZappedIn3).to.be.gt(rewardPerShareZappedIn2);
+
+        })
         // it("userRewardsOfInvestedProtocols should be reset to 0 after redeem()", async function () {
         //     await (await portfolioContract.connect(wallet).redeem(portfolioShares, wallet.address, fakePendleZapOut, { gasLimit: gasLimit })).wait();
         //     expect(userRewardsOfInvestedProtocols).to.equal(0);
