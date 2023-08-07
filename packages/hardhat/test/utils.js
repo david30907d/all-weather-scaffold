4fc8790080fb26ee784354fe5559f9bb83d2482c
davidtnfsh <davidtnfsh@gmail.com>
2023-08-07 17:21:54 +0800

    [refactor](beforeEach): follow DRY principle for beforeEach block

--- packages/hardhat/test/utils.js
+++ packages/hardhat/test/utils.js
@@ -1,11 +1,12 @@
 const { config } = require('dotenv');
 const { network, ethers } = require("hardhat");
 const got = require('got');
+const fs = require('fs');
 const { Router, toAddress, MarketEntity } = require('@pendle/sdk-v2');
-const {Squid} = require('@0xsquid/sdk');
+const { Squid } = require('@0xsquid/sdk');
 
 config();
-const getSDK = ()  => {
+const getSDK = () => {
   const squid = new Squid({
     baseUrl: "https://api.0xsquid.com"
   });
@@ -37,7 +38,7 @@ async function getUserEthBalance(address) {
   return await provider.getBalance(address);
 }
 
-async function getPendleZapInData(chainId, poolAddress, amount, slippage, tokenInAddress="0x82aF49447D8a07e3bd95BD0d56f35241523fBab1") {
+async function getPendleZapInData(chainId, poolAddress, amount, slippage, tokenInAddress = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1") {
   const provider = new ethers.providers.JsonRpcProvider(process.env.API_URL);
   const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   const router = Router.getRouterWithKyberAggregator({
@@ -45,24 +46,24 @@ async function getPendleZapInData(chainId, poolAddress, amount, slippage, tokenI
     provider,
     signer,
   });
-  
+
   const GLP_POOL_ADDRESS = toAddress(poolAddress);
   const TOKEN_IN_ADDRESS = toAddress(tokenInAddress);
   return await router.addLiquiditySingleToken(
-      GLP_POOL_ADDRESS,
-      TOKEN_IN_ADDRESS,
-      amount,
-      slippage,
-      { method: 'extractParams' }
+    GLP_POOL_ADDRESS,
+    TOKEN_IN_ADDRESS,
+    amount,
+    slippage,
+    { method: 'extractParams' }
   );
 }
-async function getPendleZapOutData(chainId, poolAddress, tokenOutAddress, amount, slippage){
+async function getPendleZapOutData(chainId, poolAddress, tokenOutAddress, amount, slippage) {
   const provider = new ethers.providers.JsonRpcProvider(process.env.API_URL);
   const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   const marketContract = new MarketEntity(toAddress(poolAddress), {
-      chainId: chainId,
-      provider,
-      signer: signer
+    chainId: chainId,
+    provider,
+    signer: signer
   });
   const router = Router.getRouterWithKyberAggregator({
     chainId: chainId,
@@ -72,7 +73,7 @@ async function getPendleZapOutData(chainId, poolAddress, tokenOutAddress, amount
   // TODO(david): ask pendle team about this. Is it possible to extract Param before approving contract?
   // await marketContract.approve(router.address, amount).then((tx)=> tx.wait());
   // await marketContract.approve(router.address, ethers.BigNumber.from('115792089237316195423570985008687907853269984665640564039457')).then((tx) => tx.wait());
-  
+
   return await router.removeLiquiditySingleToken(
     toAddress(poolAddress),
     amount,
@@ -84,18 +85,18 @@ async function getPendleZapOutData(chainId, poolAddress, tokenOutAddress, amount
 
 const getLiFiCrossChainContractCallCallData = async (fromChain, fromToken, fromAddress, toChain, toToken, toAmount, crossChainTransaction, toContractGasLimit, contractOutputsToken) => {
   const quoteRequest = {
-      fromChain,
-      fromToken,
-      fromAddress,
-      toChain,
-      toToken,
-      toAmount,
-      toContractAddress: crossChainTransaction.to,
-      toContractCallData: crossChainTransaction.data,
-      toContractGasLimit,
-      contractOutputsToken,
-    };
-  
+    fromChain,
+    fromToken,
+    fromAddress,
+    toChain,
+    toToken,
+    toAmount,
+    toContractAddress: crossChainTransaction.to,
+    toContractCallData: crossChainTransaction.data,
+    toContractGasLimit,
+    contractOutputsToken,
+  };
+
   const response = await axios.post('https://li.quest/v1/quote/contractCall', quoteRequest);
   return response.data.transactionRequest.data;
 };
@@ -125,6 +126,73 @@ const getSquidCrossChainContractCallCallData = async (fromChain, toChain, fromTo
     'customContractCalls[1][callData]': customContractCalls[1]['callData'],
   })
 };
+
+async function getBeforeEachSetUp() {
+  wallet = await ethers.getImpersonatedSigner(myImpersonatedWalletAddress);
+  dpxSLP = await ethers.getContractAt('IERC20Uniswap', sushiSwapDpxLpTokenAddress);
+  weth = await ethers.getContractAt('IWETH', wethAddress);
+  dpxToken = await ethers.getContractAt("MockDAI", dpxTokenAddress);
+  fsGLP = await ethers.getContractAt("IERC20", fsGLPAddress);
+  pendleGlpMarketLPT = await ethers.getContractAt("IERC20", glpMarketPoolAddress);
+  pendleGDAIMarketLPT = await ethers.getContractAt("IERC20", gDAIMarketPoolAddress);
+  pendleToken = await ethers.getContractAt("IERC20", pendleTokenAddress);
+  daiToken = await ethers.getContractAt("IERC20", daiAddress);
+  gDAIToken = await ethers.getContractAt("IERC20", gDAIAddress);
+  // we can check our balance in equilibria with this reward pool
+  dGDAIRewardPool = await ethers.getContractAt("IERC20", gDAIRewardPoolAddress);
+  multiFeeDistribution = await ethers.getContractAt("IMultiFeeDistribution", multiFeeDistributionAddress);
+  await weth.connect(wallet).deposit({ value: ethers.utils.parseEther("1"), gasLimit });
+
+
+  const DpxArbitrumVault = await ethers.getContractFactory("DpxArbitrumVault");
+  dpxVault = await DpxArbitrumVault.deploy(dpxSLP.address, sushiMiniChefV2Address, sushiPid);
+  await dpxVault.deployed();
+
+  const EquilibriaGlpVault = await ethers.getContractFactory("EquilibriaGlpVault");
+  equilibriaGlpVault = await EquilibriaGlpVault.deploy(pendleGlpMarketLPT.address, "Equilibria-GLP", "ALP-EQB-GLP");
+  await equilibriaGlpVault.deployed();
+
+  const EquilibriaGDAIVault = await ethers.getContractFactory("EquilibriaGDAIVault");
+  equilibriaGDAIVault = await EquilibriaGDAIVault.deploy(pendleGDAIMarketLPT.address, "Equilibria-GDAI", "ALP-EQB-GDAI");
+  await equilibriaGDAIVault.deployed();
+
+  const PermanentPortfolioLPToken = await ethers.getContractFactory("PermanentPortfolioLPToken");
+  portfolioContract = await PermanentPortfolioLPToken.connect(wallet).deploy(weth.address, dpxVault.address, equilibriaGlpVault.address, equilibriaGDAIVault.address);
+  await portfolioContract.connect(wallet).deployed();
+  await portfolioContract.setVaultAllocations([{
+    protocol: "SushSwap-DpxETH", percentage: 25,
+  }, {
+    protocol: "Equilibria-GLP", percentage: 25
+  }, {
+    protocol: "Equilibria-GDAI", percentage: 25
+  }
+  ]).then((tx) => tx.wait());
+  await (await weth.connect(wallet).approve(portfolioContract.address, end2endTestingAmount, { gasLimit })).wait();
+
+  try {
+    console.log("read 1inch calldata and pendle calldata from json file")
+    oneInchSwapDataForDpx = JSON.parse(fs.readFileSync('oneInchSwapDataForDpx.json', 'utf8'));
+    oneInchSwapDataForGDAI = JSON.parse(fs.readFileSync('oneInchSwapDataForGDAI.json', 'utf8'));
+    pendleGDAIZapInData = JSON.parse(fs.readFileSync('pendleGDAIZapInData.json', 'utf8'));
+    pendleGLPZapInData = JSON.parse(fs.readFileSync('pendleGLPZapInData.json', 'utf8'));
+  } catch (err) {
+    console.error('json file not found, get new 1inch calldata and pendle calldata');
+    oneInchSwapDataForDpx = await fetch1InchSwapData(weth.address, dpxTokenAddress, amountAfterChargingFee.div(8), dpxVault.address, 50);
+    fs.writeFileSync('oneInchSwapDataForDpx.json', JSON.stringify(oneInchSwapDataForDpx, null, 2), 'utf8')
+
+    oneInchSwapDataForGDAI = await fetch1InchSwapData(weth.address, daiToken.address, amountAfterChargingFee.div(4), equilibriaGDAIVault.address, 50);
+    fs.writeFileSync('oneInchSwapDataForGDAI.json', JSON.stringify(oneInchSwapDataForGDAI, null, 2), 'utf8')
+
+    // oneInchSwapDataForGDAI.toAmount).div(2): due to the 1inch slippage, need to multiple by 0.95 to pass pendle zap in
+    pendleGDAIZapInData = await getPendleZapInData(42161, gDAIMarketPoolAddress, ethers.BigNumber.from(oneInchSwapDataForGDAI.toAmount).mul(50).div(100), 0.2, daiToken.address);
+    fs.writeFileSync('pendleGDAIZapInData.json', JSON.stringify(pendleGDAIZapInData, null, 2), 'utf8')
+
+    pendleGLPZapInData = await getPendleZapInData(42161, glpMarketPoolAddress, amountAfterChargingFee.div(4), 0.99);
+    fs.writeFileSync('pendleGLPZapInData.json', JSON.stringify(pendleGLPZapInData, null, 2), 'utf8')
+  }
+  portfolioShares = amountAfterChargingFee.div(await portfolioContract.unitOfShares());
+  return [wallet, weth, oneInchSwapDataForDpx, oneInchSwapDataForGDAI, pendleGDAIZapInData, pendleGLPZapInData, portfolioShares, dpxVault, equilibriaGDAIVault, equilibriaGlpVault, portfolioContract];
+}
 
 // common config
 // Rich guy
@@ -160,7 +228,7 @@ const radiantDlpAddress = "0x32dF62dc3aEd2cD6224193052Ce665DC18165841";
 const radiantLendingPoolAddress = "0xF4B1486DD74D07706052A33d31d7c0AAFD0659E1";
 const radiantLockZapPoolAddress = "0x8991C4C347420E476F1cf09C03abA224A76E2997";
 const multiFeeDistributionAddress = "0x76ba3eC5f5adBf1C58c91e86502232317EeA72dE";
-const radiantRTokens = ["0xd69D402D1bDB9A2b8c3d88D98b9CEaf9e4Cd72d9",          "0x48a29E756CC1C097388f3B2f3b570ED270423b3d",          "0x0D914606f3424804FA1BbBE56CCC3416733acEC6",          "0x0dF5dfd95966753f01cb80E76dc20EA958238C46",          "0x42C248D137512907048021B30d9dA17f48B5b7B2",          "0x2dADe5b7df9DA3a7e1c9748d169Cd6dFf77e3d01"]
+const radiantRTokens = ["0xd69D402D1bDB9A2b8c3d88D98b9CEaf9e4Cd72d9", "0x48a29E756CC1C097388f3B2f3b570ED270423b3d", "0x0D914606f3424804FA1BbBE56CCC3416733acEC6", "0x0dF5dfd95966753f01cb80E76dc20EA958238C46", "0x42C248D137512907048021B30d9dA17f48B5b7B2", "0x2dADe5b7df9DA3a7e1c9748d169Cd6dFf77e3d01"]
 
 // radiant-bsc
 const wbnbAddress = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
@@ -273,5 +341,6 @@ module.exports = {
   radiantRTokens,
   claimableRewardsTestData,
   amountAfterChargingFee,
-  claimableRewardsTestDataForPermanentPortfolio
+  claimableRewardsTestDataForPermanentPortfolio,
+  getBeforeEachSetUp
 };
